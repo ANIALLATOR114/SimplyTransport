6b08bd32ea1f499aae50484d43f636d29f0c6e33
ANIALLATOR114 <116189545+ANIALLATOR114@users.noreply.github.com>
2023-10-29 23:27:01 +0000

    tests

--- SimplyTransport/cli.py
+++ SimplyTransport/cli.py
@@ -138,3 +138,15 @@ class CLIPlugin(CLIPluginProtocol):
 
             finish: float = time.perf_counter()
             console.print(f"\n[blue]Finished import in {round(finish-start, 2)} second(s)")
+
+
+        @cli.command(name="create_tables", help="Creates the database tables")
+        def create_tables():
+            """Creates the database tables"""
+
+            console = Console()
+            console.print("Creating database tables...")
+
+            from SimplyTransport.lib.db import services as db_services
+
+            db_services.create_database_sync()
