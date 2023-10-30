af69eac280d2bfb98e932b6e1c60820435ed45df
ANIALLATOR114 <116189545+ANIALLATOR114@users.noreply.github.com>
2023-10-30 11:53:02 +0000

    Update test_calendar_date.py

--- tests/integration/gtfs_controllers/test_calendar_date.py
+++ tests/integration/gtfs_controllers/test_calendar_date.py
@@ -6,7 +6,6 @@ def test_calendar_date_all(client: AsyncClient)-> None:
     assert response.status_code == 200
     response_json = response.json()
     assert len(response_json) == 238
-    assert response_json[0]["id"] == 3571
     assert response_json[0]["service_id"] == "27"
     assert response_json[0]["date"] == "2023-11-24"
     assert response_json[0]["exception_type"] == "added"
@@ -25,7 +24,6 @@ def test_get_calendar_date_by_id(client: AsyncClient)-> None:
     response = client.get("api/v1/calendardate/3571")
     assert response.status_code == 200
     response_json = response.json()
-    assert response_json["id"] == 3571
     assert response_json["service_id"] == "27"
     assert response_json["date"] == "2023-11-24"
     assert response_json["exception_type"] == "added"
@@ -34,7 +32,6 @@ def test_get_calendar_date_by_id(client: AsyncClient)-> None:
     response = client.get("api/v1/calendardate/3572")
     assert response.status_code == 200
     response_json = response.json()
-    assert response_json["id"] == 3572
     assert response_json["service_id"] == "124"
     assert response_json["date"] == "2023-10-30"
     assert response_json["exception_type"] == "removed"
@@ -46,7 +43,6 @@ def test_get_active_calendar_dates_on_date(client: AsyncClient)-> None:
     assert response.status_code == 200
     response_json = response.json()
     assert len(response_json) == 24
-    assert response_json[0]["id"] == 3572
     assert response_json[0]["service_id"] == "124"
     assert response_json[0]["date"] == "2023-10-30"
     assert response_json[0]["exception_type"] == "removed"
@@ -58,7 +54,6 @@ def test_get_active_calendar_dates_on_date(client: AsyncClient)-> None:
     assert response.status_code == 200
     response_json = response.json()
     assert len(response_json) == 9
-    assert response_json[0]["id"] == 3574
     assert response_json[0]["service_id"] == "154"
     assert response_json[0]["date"] == "2024-01-01"
     assert response_json[0]["exception_type"] == "removed"
