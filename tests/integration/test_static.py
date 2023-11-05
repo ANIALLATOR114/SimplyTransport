9fcd2ec821b64769c58e4659b3b125a115d31bce
ANIALLATOR114 <116189545+ANIALLATOR114@users.noreply.github.com>
2023-11-05 00:43:51 +0000

    fix test

--- tests/integration/test_static.py
+++ tests/integration/test_static.py
@@ -4,7 +4,11 @@ from httpx import AsyncClient
 def test_favicon(client: AsyncClient) -> None:
     response = client.get("/favicon.ico")
     assert response.status_code == 200
-    assert response.headers["content-type"] == "image/x-icon"
+    content_types = [
+        "image/vnd.microsoft.icon",
+        "image/x-icon",
+    ]
+    assert response.headers["content-type"] in content_types
 
 
 def test_htmx_js(client: AsyncClient) -> None:
