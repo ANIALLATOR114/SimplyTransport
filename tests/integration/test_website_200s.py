efe51562412f31f405837a653e211ed529afc43d
ANIALLATOR114 <116189545+ANIALLATOR114@users.noreply.github.com>
2023-12-23 00:30:14 +0000

    fix test

--- tests/integration/test_website_200s.py
+++ tests/integration/test_website_200s.py
@@ -5,7 +5,6 @@ urls = ["/", "/events/", "/stops/", "/routes/", "/apidocs", "/about"]
 
 
 @pytest.mark.parametrize("url", urls)
-@pytest.mark.asyncio
 def test_url_200(client: TestClient, url: str) -> None:
     response = client.get(url)
     assert response.status_code == 200
