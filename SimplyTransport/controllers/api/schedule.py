fe56e1fedbcc6414a48bcdeff3291a3724eeb4bb
ANIALLATOR114 <116189545+ANIALLATOR114@users.noreply.github.com>
2024-01-25 00:53:08 +0000

    Fix API validation + more tests

--- SimplyTransport/controllers/api/schedule.py
+++ SimplyTransport/controllers/api/schedule.py
@@ -2,8 +2,10 @@ from datetime import datetime, timedelta, time
 
 from litestar import Controller, get
 from litestar.di import Provide
-from litestar.exceptions import NotFoundException, ValidationException
+from litestar.exceptions import ValidationException
 from litestar.params import Parameter
+
+from ...lib.time_date_conversions import return_time_difference
 from ...domain.enums import DayOfWeek
 
 from ...domain.services.schedule_service import (
@@ -21,16 +23,16 @@ class ScheduleController(Controller):
         "schedule_service": Provide(provide_schedule_service),
     }
 
-    @get("/{stop_id:str}", summary="Get schedule for a stop", raises=[NotFoundException])
+    @get("/{stop_id:str}", summary="Get schedule for a stop", raises=[ValidationException])
     async def get_schedule_by_stop_id(
         self,
         schedule_service: ScheduleService,
         stop_id: str,
         start_time: time
-        | None = Parameter(required=False, description="Start time, defaults to 10 minutes ago"),
+        | None = Parameter(required=False, description="Start time, defaults to 10 minutes ago\n\nExample: 10:00:00"),
         end_time: time
-        | None = Parameter(required=False, description="End time, defaults to 60 minutes from now"),
-        day: DayOfWeek = Parameter(required=False, description="Day of week"),
+        | None = Parameter(required=False, description="End time, defaults to 60 minutes from now\n\nExample: 11:00:00"),
+        day: DayOfWeek | None = Parameter(required=False, description="Day of week, defaults to today"),
     ) -> list[StaticSchedule]:
         """Returns a list of schedules for the given stop_id"""
 
@@ -43,17 +45,13 @@ class ScheduleController(Controller):
         if day is None:
             day = DayOfWeek(datetime.now().weekday())
 
-        if start_time > end_time:
-            raise ValidationException("Start time cannot be after end time")
-
         if start_time == end_time:
             raise ValidationException("Start time cannot be equal to end time")
 
-        max_hours_apart = 3
-        if (end_time.hour - start_time.hour) > max_hours_apart:
-            raise ValidationException(
-                f"Start time and end time cannot be more than {max_hours_apart} hours apart"
-            )
+        max_hours_apart = 6
+        difference = return_time_difference(start_time,end_time)
+        if difference > max_hours_apart:
+            raise ValidationException(f"The difference of hours between start and end time must be at most {max_hours_apart} hours",extra={"start_time":start_time,"end_time":end_time,"hours_difference":difference})
 
         schedules = await schedule_service.get_schedule_on_stop_for_day_between_times(
             stop_id=stop_id,
