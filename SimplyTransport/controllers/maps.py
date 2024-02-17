eac277eaa1fe6f80d68a9d37e1267ac819eaabfc
ANIALLATOR114 <116189545+ANIALLATOR114@users.noreply.github.com>
2024-02-17 02:10:36 +0000

    Copy update, Compression

--- SimplyTransport/controllers/maps.py
+++ SimplyTransport/controllers/maps.py
@@ -1,6 +1,7 @@
 from advanced_alchemy import NotFoundError
-from litestar import Controller, get
+from litestar import Controller, MediaType, get, template
 from litestar.di import Provide
+from litestar.response import Template
 
 from ..domain.services.map_service import MapService, provide_map_service
 from ..lib.cache_keys import CacheKeys, key_builder_from_path
@@ -21,9 +22,9 @@ class MapsController(Controller):
         cache=86400,
         cache_key_builder=key_builder_from_path(CacheKeys.STOP_MAP_KEY_TEMPLATE, "stop_id"),
     )
-    async def map_for_stop(self, stop_id: str, map_service: MapService) -> str:
+    async def map_for_stop(self, stop_id: str, map_service: MapService) -> Template:
         try:
             stop_map = await map_service.generate_stop_map(stop_id)
         except NotFoundError:
             return "Stop not found"
-        return stop_map.render()
+        return Template(template_str=stop_map.render(),media_type=MediaType.HTML)
