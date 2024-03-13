6aaa670f891e094d2a2bdda3e609d3c4a0c60009
ANIALLATOR114 <116189545+ANIALLATOR114@users.noreply.github.com>
2024-03-13 22:26:09 +0000

    Instrument OpenTelemetry

---
+++ SimplyTransport/lib/opentelemetry.py
@@ -0,0 +1,39 @@
+
+from . import settings
+
+from litestar.contrib.opentelemetry import OpenTelemetryConfig
+
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource, SERVICE_VERSION, DEPLOYMENT_ENVIRONMENT
+
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+from opentelemetry import metrics
+from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+
+resource = Resource(attributes={
+    SERVICE_NAME: settings.app.NAME,
+    SERVICE_VERSION: settings.app.VERSION,
+    "service.debug": settings.app.DEBUG,
+    "service.log_level": settings.app.LOG_LEVEL,
+    DEPLOYMENT_ENVIRONMENT: settings.app.ENVIRONMENT,
+})
+
+traceProvider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(
+    OTLPSpanExporter(endpoint="http://localhost:4318/v1/traces")
+)
+traceProvider.add_span_processor(processor)
+trace.set_tracer_provider(traceProvider)
+
+reader = PeriodicExportingMetricReader(
+    OTLPMetricExporter(endpoint="http://localhost:4318/v1/metrics")
+)
+meterProvider = MeterProvider(resource=resource, metric_readers=[reader])
+metrics.set_meter_provider(meterProvider)
+
+open_telemetry_config = OpenTelemetryConfig()
