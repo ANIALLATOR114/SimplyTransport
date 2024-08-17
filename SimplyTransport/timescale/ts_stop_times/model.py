d07e9cc2b513b8616c04d672f4e57c0299c4f64f
ANIALLATOR114 <ANIALLATOR114@users.noreply.github.com>
2024-08-17 12:57:59 +0000

    [AUTOMATED] Format Code

--- SimplyTransport/timescale/ts_stop_times/model.py
+++ SimplyTransport/timescale/ts_stop_times/model.py
@@ -1,10 +1,10 @@
-
 from datetime import datetime
 from litestar.contrib.sqlalchemy.base import BigIntBase
 
 from sqlalchemy import DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 from pydantic import BaseModel as _BaseModel
+
 
 class BaseModel(_BaseModel):
     """Extend Pydantic's BaseModel to enable ORM mode"""
@@ -15,7 +15,9 @@ class BaseModel(_BaseModel):
 class TS_StopTimeModel(BigIntBase):
     __tablename__ = "ts_stop_times"
 
-    Timestamp : Mapped[DateTime] = mapped_column(DateTime, nullable=False, default=datetime.now, primary_key=True)
+    Timestamp: Mapped[DateTime] = mapped_column(
+        DateTime, nullable=False, default=datetime.now, primary_key=True
+    )
     # Something with stoptimes and trips etc
     # Maybe some offsets from expected times
 
