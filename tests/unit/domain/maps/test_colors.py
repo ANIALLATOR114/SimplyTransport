d851966831fdd4c23aeac0b73b647592ed664c44
ANIALLATOR114 <116189545+ANIALLATOR114@users.noreply.github.com>
2026-03-28 12:37:56 +0000

    add more colours

--- tests/unit/domain/maps/test_colors.py
+++ tests/unit/domain/maps/test_colors.py
@@ -2,5 +2,5 @@ from SimplyTransport.domain.maps.colors import Colors
 
 
 def test_to_hex():
-    assert Colors.RED.to_hex() == "#ff0000"
-    assert Colors.BLUE.to_hex() == "#3388ff"
+    assert Colors.RED.to_hex() == "#d32f2f"
+    assert Colors.BLUE.to_hex() == "#1565c0"
